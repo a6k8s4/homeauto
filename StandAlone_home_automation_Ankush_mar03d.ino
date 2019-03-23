6bfc437692da1bd2a08ec19a984ea5d2b86bbcd1
a6k8s4 <17820968+a6k8s4@users.noreply.github.com>
2019-03-23 12:27:50 +0530

    Update StandAlone_home_automation_Ankush_mar03d.ino

--- StandAlone_home_automation_Ankush_mar03d.ino
+++ StandAlone_home_automation_Ankush_mar03d.ino
@@ -1,188 +1,188 @@
-//By Ankush 
-#include <ESP8266WiFi.h>//ESP8266 Core WiFi Library    
+//By Ankush
+#include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-const char* ssid = "homeauto"; // Input your hotspot name
-const char* password = "password1234";
-IPAddress local_ip(192,143,2,1);
-IPAddress gateway(192,143,2,1);
+
+/* Put your SSID & Password */
+const char* ssid = "HomeAuto";  // Enter SSID here
+const char* password = "password1234";  //Enter Password here
+
+/* Put IP Address details */
+IPAddress local_ip(192,143,1,1);
+IPAddress gateway(192,143,1,1);
 IPAddress subnet(255,255,255,0);
-ESP8266WebServer server(80); // Set web server port number to 80
-String header;// Variable to store the HTTP request
-// Auxiliar variables to store the current output state
-String relay1State = "off";
-String relay2State = "off";
-String relay3State = "off";
-String relay4State = "off";
-// Assign output variables to GPIO pins
-const int relay1 = 5; // GPIO5 D1
-const int relay2 = 4; // GPIO4 D2
-const int relay3 = 0; // GPIO0 D3
-const int relay4 = 2; // GPIO2 D4
+
+ESP8266WebServer server(80);
+
+// Initializing Relay Input Pins.
+// Using NC(Normally Closed) connection for connecting the devices.
+
+const int SWITCH1pin = 14; //GPIO Pin 14 or D5
+bool SWITCH1status = LOW;
+
+const int SWITCH2pin = 12;
+bool SWITCH2status = LOW; //GPIO Pin 12 or D6
+
+const int SWITCH3pin = 13;
+bool SWITCH3status = LOW; //GPIO Pin 13 or D7
+
+const int SWITCH4pin = 15;
+bool SWITCH4status = LOW; //GPIO Pin 15 or D8
+
 
 void setup() {
   Serial.begin(115200);
   
-  // Initialize the output variables as outputs
-  pinMode(relay1, OUTPUT);
-  pinMode(relay2, OUTPUT);
-  pinMode(relay3, OUTPUT);
-  pinMode(relay4, OUTPUT);
-  // Set outputs to HIGH. relay active LOW
-  digitalWrite(relay1, HIGH);
-  digitalWrite(relay2, HIGH);
-  digitalWrite(relay3, HIGH);
-  digitalWrite(relay4, HIGH);
-  Serial.println("Setting Access Point........");
+  // initialize pin as OUTPUT
+  pinMode(SWITCH1pin, OUTPUT);
+  pinMode(SWITCH2pin, OUTPUT);
+  pinMode(SWITCH3pin, OUTPUT);
+  pinMode(SWITCH4pin, OUTPUT);
+
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(local_ip, gateway, subnet);
-  delay(150);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
+  delay(100);
+  
+  server.on("/", handle_OnConnect);
+  server.on("/switch1on", handle_switch1on);
+  server.on("/switch1off", handle_switch1off);
+  server.on("/switch2on", handle_switch2on);
+  server.on("/switch2off", handle_switch2off);
+  server.on("/switch3on", handle_switch3on);
+  server.on("/switch3off", handle_switch3off);
+  server.on("/switch4on", handle_switch4on);
+  server.on("/switch4off", handle_switch4off);
+  server.onNotFound(handle_NotFound);
+  
   server.begin();
   Serial.println("Server started");
 }
 void loop() {
-  WiFiClient client = server.available();   // Listen for incoming clients
+  server.handleClient();
+  if(SWITCH1status)
+  {digitalWrite(SWITCH1pin, HIGH);}
+  else
+  {digitalWrite(SWITCH1pin, LOW);}
+  
+  if(SWITCH2status)
+  {digitalWrite(SWITCH2pin, HIGH);}
+  else
+  {digitalWrite(SWITCH2pin, LOW);}
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
+  if(SWITCH3status)
+  {digitalWrite(SWITCH3pin, HIGH);}
+  else
+  {digitalWrite(SWITCH3pin, LOW);}
 
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0)
-            {
-              Serial.println("GPIO 5 on");
-              relay1State = "on";
-              digitalWrite(relay1, LOW);
-            }
-            else if (header.indexOf("GET /5/off") >= 0)
-            {
-              Serial.println("GPIO 5 off");
-              relay1State = "off";
-              digitalWrite(relay1, HIGH);
-            }
-            else if (header.indexOf("GET /4/on") >= 0) {
-              Serial.println("GPIO 4 on");
-              relay2State = "on";
-              digitalWrite(relay2, LOW);
-            }
-            else if (header.indexOf("GET /4/off") >= 0) {
-              Serial.println("GPIO 4 off");
-              relay2State = "off";
-              digitalWrite(relay2, HIGH);
-            }
-            else if (header.indexOf("GET /0/on") >= 0)
-            {
-              Serial.println("GPIO 0 on");
-              relay3State = "on";
-              digitalWrite(relay3, LOW);
-            }
-            else if (header.indexOf("GET /0/off") >= 0)
-            {
-              Serial.println("GPIO 0 off");
-              relay3State = "off";
-              digitalWrite(relay3, HIGH);
-            }
-            else if (header.indexOf("GET /2/on") >= 0) {
-              Serial.println("GPIO 2 on");
-              relay4State = "on";
-              digitalWrite(relay4, LOW);
-            }
-            else if (header.indexOf("GET /2/off") >= 0) {
-              Serial.println("GPIO 2 off");
-              relay4State = "off";
-              digitalWrite(relay4, HIGH);
-            }
+  if(SWITCH4status)
+  {digitalWrite(SWITCH4pin, HIGH);}
+  else
+  {digitalWrite(SWITCH4pin, LOW);}
+}
 
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<title> Controll Server</title>");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; border-style: ridge; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".switch { background-color: #00FF00; border: 2px solid #008CBA; color: white; padding: 12px 24px;");
-            client.println("text-decoration: none; font-size: 20px; margin: 2px; cursor: pointer;}");
-            client.println(".switch2 {background-color: #FF0000;}</style></head>");
+void handle_OnConnect() {
+  SWITCH1status = LOW;
+  SWITCH2status = LOW;
+  SWITCH3status = LOW;
+  SWITCH4status = LOW;
+  
+  Serial.println("GPIO 14 Status: OFF | GPIO 12 Status: OFF | GPIO 13 Status: OFF | GPIO 15 Status: OFF");
+  server.send(200, "text/html", SendHTML(SWITCH1status,SWITCH2status,SWITCH3status,SWITCH4status)); 
+}
 
-            // Web Page Heading
-            client.println("<body><h1>Command Center</h1>");
+void handle_switch1on() {
+  SWITCH1status = HIGH;
+  Serial.println("GPIO 14 Status: ON");
+  server.send(200, "text/html", SendHTML(true,SWITCH2status)); 
+}
 
-            // Display current state, and ON/OFF buttons for GPIO 5
-            client.println("<p>Switch 1- State " + relay1State + "</p>");
-            // If the relay1State is off, it displays the ON button
-            if (relay1State == "off") {
-              client.println("<p><a href=\"/5/on\"><button class=\"switch\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/5/off\"><button class=\"switch switch2\">OFF</button></a></p>");
-            }
+void handle_switch1off() {
+  SWITCH1status = LOW;
+  Serial.println("GPIO 14 Status: OFF");
+  server.send(200, "text/html", SendHTML(false,SWITCH2status)); 
+}
 
-            // Display current state, and ON/OFF buttons for GPIO 4
-            client.println("<p>Switch 2 - State " + relay2State + "</p>");
-            // If the relay2State is off, it displays the ON button
-            if (relay2State == "off") {
-              client.println("<p><a href=\"/4/on\"><button class=\"switch\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/4/off\"><button class=\"switch switch2\">OFF</button></a></p>");
-            }
+void handle_switch2on() {
+  SWITCH2status = HIGH;
+  Serial.println("GPIO 12 Status: ON");
+  server.send(200, "text/html", SendHTML(SWITCH1status,true)); 
+}
 
-            // Display current state, and ON/OFF buttons for GPIO 0
-            client.println("<p>Switch 3 - State " + relay3State + "</p>");
-            // If the relay1State is off, it displays the ON button
-            if (relay3State == "off") {
-              client.println("<p><a href=\"/0/on\"><button class=\"switch\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/0/off\"><button class=\"switch switch2\">OFF</button></a></p>");
-            }
+void handle_switch2off() {
+  SWITCH2status = LOW;
+  Serial.println("GPIO 12 Status: OFF");
+  server.send(200, "text/html", SendHTML(SWITCH1status,false)); 
+}
 
-            // Display current state, and ON/OFF buttons for GPIO 2
-            client.println("<p>Switch 4 - State " + relay4State + "</p>");
-            // If the relay2State is off, it displays the ON button
-            if (relay4State == "off") {
-              client.println("<p><a href=\"/2/on\"><button class=\"switch\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/2/off\"><button class=\"switch switch2\">OFF</button></a></p>");
-            }
+void handle_switch3on() {
+  SWITCH3status = HIGH;
+  Serial.println("GPIO 13 Status: ON");
+  server.send(200, "text/html", SendHTML(SWITCH4status,true)); 
+}
 
-            client.println("</body></html>");
+void handle_switch3off() {
+  SWITCH3status = LOW;
+  Serial.println("GPIO 13 Status: OFF");
+  server.send(200, "text/html", SendHTML(SWITCH4status,false)); 
+}
 
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("User disconnected.");
-    Serial.println("");
-  }
+void handle_switch4on() {
+  SWITCH4status = HIGH;
+  Serial.println("GPIO 15 Status: ON");
+  server.send(200, "text/html", SendHTML(SWITCH3status,true)); 
+}
+
+void handle_switch4off() {
+  SWITCH4status = LOW;
+  Serial.println("GPIO 15 Status: OFF");
+  server.send(200, "text/html", SendHTML(SWITCH3status,false)); 
+}
+
+
+
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(uint8_t switch1stat,uint8_t switch2stat,uint8_t switch3stat,uint8_t switch4stat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>Control Panel</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: border-style: ridge; inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr +=".button {display: block;width: 80px;border: 2px solid #008CBA; color: white; padding: 12px 24px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+  ptr +=".button-on {background-color: #00FF00;}\n";
+  ptr +=".button-on:active {background-color: #00FF00;}\n";
+  ptr +=".button-off {background-color: #FF0000;}\n";
+  ptr +=".button-off:active {background-color: #FF0000;}\n";
+  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>Command Center </h1>\n";
+  
+   if(switch1stat)
+  {ptr +="<p>SWITCH1 Status: ON</p><a class=\"button button-off\" href=\"/switch1off\">OFF</a>\n";}
+  else
+  {ptr +="<p>SWITCH1 Status: OFF</p><a class=\"button button-on\" href=\"/switch1on\">ON</a>\n";}
+
+  if(switch2stat)
+  {ptr +="<p>SWITCH2 Status: ON</p><a class=\"button button-off\" href=\"/switch2off\">OFF</a>\n";}
+  else
+  {ptr +="<p>SWITCH2 Status: OFF</p><a class=\"button button-on\" href=\"/switch2on\">ON</a>\n";}
+
+  if(switch3stat)
+  {ptr +="<p>SWITCH3 Status: ON</p><a class=\"button button-off\" href=\"/switch3off\">OFF</a>\n";}
+  else
+  {ptr +="<p>SWITCH3 Status: OFF</p><a class=\"button button-on\" href=\"/switch3on\">ON</a>\n";}
+
+  if(switch4stat)
+  {ptr +="<p>SWITCH4 Status: ON</p><a class=\"button button-off\" href=\"/switch4off\">OFF</a>\n";}
+  else
+  {ptr +="<p>SWITCH4 Status: OFF</p><a class=\"button button-on\" href=\"/switch4on\">ON</a>\n";}
+
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
 }
